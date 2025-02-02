dc391309a2da833715b9fadfa708cac59d40db37
Maxime Perreault <maxime.perreault@hotmail.fr>
2025-02-02 17:10:08 -0500

    added lockscreen logic

--- TimeBlend/Assets/Scripts/UI/UIcontroller.cs
+++ TimeBlend/Assets/Scripts/UI/UIcontroller.cs
@@ -9,41 +9,65 @@ public class UIcontroller : MonoBehaviour
     public int currentScreen = 0;
 
     public TMP_Text menuText;
+    public GameObject menuObject;
+    public GameObject lockObject;
 
     public GameObject leftButtonHighlight;
     public GameObject rightButtonHighlight;
-    public float highlightTiming = 0.5f;
 
-    public GameObject mapParent;
+    public GameObject lockParent;
     public GameObject messagesParent;
+    public GameObject mapParent;
+
 
     private bool isLeftSelected = false;
+    private bool isUnlocked = false;
 
     void Start()
     {
         // Create screen objects
+        screens.Add(new ScreenData("lock", lockParent));
         screens.Add(new ScreenData("msgs", messagesParent));
         screens.Add(new ScreenData("map", mapParent));
-        screens.Add(new ScreenData("test01", mapParent));
-        screens.Add(new ScreenData("test02", mapParent));
+
+        // make sure the right objects are hidden or shown
+        lockObject.SetActive(true);
+        lockParent.SetActive(true);
+        menuObject.SetActive(false);
+        leftButtonHighlight.SetActive(false);
+        rightButtonHighlight.SetActive(false);
+        foreach (ScreenData screen in screens)
+        {
+            screen.emptyParent.SetActive(false);
+        }
         
         UpdateContent();
     }
 
     void Update()
-    {
-        // Detect arrow key presses
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+    {   
+        if (!isUnlocked) 
         {
-            HighlightRightButton();
+            if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
+            {
+                UnlockPhone();
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        else 
         {
-            HighlightLeftButton();
-        }
-        else if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
-        {
-            ConfirmSelection();
+            // Detect arrow key presses
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                HighlightRightButton();
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                HighlightLeftButton();
+            }
+            else if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
+            {
+                ConfirmSelection();
+            }   
         }
     }
 
@@ -77,9 +101,27 @@ public class UIcontroller : MonoBehaviour
     {
         rightButtonHighlight.SetActive(false);
     }
+    
+    public void UnlockPhone()
+    {
+        // keep status of lockscreen opened
+        isUnlocked = true;
+        // remove lockscreen from the list of screens available
+        screens.RemoveAt(0);
+        
+        // hide the clock + lockscreen and show the menu
+        lockParent.SetActive(false);
+        lockObject.SetActive(false);
+        menuObject.SetActive(true);
 
+        // decide which screen is opened first
+        currentScreen = 0;
+        // update the content of the menu and main screen
+        UpdateContent();
+    }
     public void UpdateContent()
     {
+        // update the content of the menu and main screen
         menuText.text = screens[currentScreen].title;
         screens[currentScreen].emptyParent.SetActive(true);
     }
