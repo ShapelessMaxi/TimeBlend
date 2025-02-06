33902fdd48c470b889fad6f6059779e191dd2cb0
ShapelessMaxi <maxime.perreault@hotmail.fr>
2025-02-06 17:57:00 -0500

    color to uv with substance designer
    now have to figure out a way to ``automate` or at least make the workflow more fluid

--- TimeBlend/Assets/Scripts/Shaders/AnimatedShaders.cs
+++ TimeBlend/Assets/Scripts/Shaders/AnimatedShaders.cs
@@ -4,7 +4,6 @@ public class AnimatedShaders : MonoBehaviour
 {
     public Material DeviceScreen;
     public Material DeviceHighlights;
-    public Material DeviceEnvGround;
     public Material DeviceEnvFog;
 
     void Start()
@@ -12,8 +11,6 @@ public class AnimatedShaders : MonoBehaviour
         DeviceScreen.SetFloat("_Speed", 0.5f);
 
         DeviceHighlights.SetFloat("_Speed", 1.0f);
-
-        DeviceEnvGround.SetVector("_PositionAnimation", new Vector4(0f, 0.2f, 0f, 0f));
 
         DeviceEnvFog.SetFloat("_layer1_Speed", 0.5f);
         DeviceEnvFog.SetFloat("_layer2_Speed", 1.0f);
@@ -23,7 +20,6 @@ public class AnimatedShaders : MonoBehaviour
     {
         DeviceScreen.SetFloat("_Speed", 0f);
         DeviceHighlights.SetFloat("_Speed", 0f);
-        DeviceEnvGround.SetVector("_PositionAnimation", Vector4.zero);
         DeviceEnvFog.SetFloat("_layer1_Speed", 0f);
         DeviceEnvFog.SetFloat("_layer2_Speed", 0f);
     }
