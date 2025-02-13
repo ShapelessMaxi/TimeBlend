ace00ecfac58ef3001b0f5b91d84b0e2ca739e1f
Maxime Perreault <maxime.perreault@hotmail.fr>
2025-02-12 21:36:06 -0500

    ui concept progress

--- TimeBlend/Assets/Scripts/UI/UIcontroller.cs
+++ TimeBlend/Assets/Scripts/UI/UIcontroller.cs
@@ -32,7 +32,8 @@ public class UIcontroller : MonoBehaviour
 {
     // access to list of sent messages
     public MessageSender messageSender;
-    public List<MessageData> receivedMessagesMod = new List<MessageData>();
+    public List<MessageData> messagesShifting = new List<MessageData>();
+    public List<MessageData> messagesReversed = new List<MessageData>();
 
     // list containing all Screen objects
     private List<ScreenData> screens = new List<ScreenData>();
@@ -178,7 +179,7 @@ public class UIcontroller : MonoBehaviour
                         if (messageSelected)
                         {
                             // display notifications (true for shift bool)
-                            NotifDisplay(receivedMessagesMod, true);
+                            NotifDisplay(messagesShifting, true);
                         }
                         
                         // no message selected (coming from menu)
@@ -186,10 +187,10 @@ public class UIcontroller : MonoBehaviour
                         else 
                         {
                             // re copy the list of received messages to be modified
-                            receivedMessagesMod = new List<MessageData>(messageSender.sentMessages);
+                            messagesShifting = new List<MessageData>(messageSender.sentMessages);
 
                             // display the notifications
-                            NotifDisplay(receivedMessagesMod, false);
+                            NotifDisplay(messagesShifting, false);
 
                             // status check
                             messageSelected = true;
@@ -226,64 +227,65 @@ public class UIcontroller : MonoBehaviour
     }
     
     // handle the logic of displaying the message notifications
-    void NotifDisplay(List<MessageData> messageList, bool shift)
+    void NotifDisplay(List<MessageData> messages, bool shift)
     {
         // Compte le nombre de messages recus
-        int messageCount = messageList.Count;
+        int messagesCount = messages.Count;
         
         // Afficher un maximum de 3 notification
         // Retourne le nombre entier le plus petit entre 3 et le nombres de messages recus
-        int displayCount = Mathf.Min(3, messageCount);
+        int displayCount = Mathf.Min(3, messagesCount);
         
         // Cacher les notifications superflues
         notifA.SetActive(displayCount > 0);
         notifB.SetActive(displayCount > 1);
         notifC.SetActive(displayCount > 2);
         
-        int shiftingIndex = 0;
+        // faire une copy de messages
+        messagesReversed = new List<MessageData>(messageSender.sentMessages);
+        // reverser l'ordre de la liste
+        messagesReversed.Reverse();
+        
+        // Check if we need to shift the array
+        // 0: static, 1: up, -1: down
+        int shiftDirection = 0;
         if (shift) 
         {
-          shiftingIndex = 1;
+          shiftDirection = 1;
         }
-    
+
+        // WriteNotification(messages, shiftDirection);
         // count up to 3 (displayed notification)
         for (int i = 0; i < displayCount; i++)
         {
-            // Access messages in reverse order (newest first)
-            int msgIndex = messageCount - 1 - i - shiftingIndex;
-            // i = 0 = A, 1 = B, 2 = C
-            WriteNotification(i, messageList[msgIndex]);
-        }
-    }
-    
-    // Write the correct info on the correct game object
-    void WriteNotification(int index, MessageData message)
-    {
-        switch(index) {
-            case 0:
-                notifA.SetActive(true);
-                notifA_name.text = message.senderName;
-                notifA_time.text = clockText.text;
-                notifA_msg.text = message.fullMessage.Substring(0, 12);
-                return;
-
-            case 1:
-                notifB.SetActive(true);
-                notifB_name.text = message.senderName;
-                notifB_time.text = clockText.text;
-                notifB_msg.text = message.fullMessage.Substring(0, 12);
-                return;
+            // keep track of current messages
+            MessageData currentMessage = messagesReversed[i];
+            string truncatedMessage = currentMessage.fullMessage.Substring(0, 12);
             
-            case 2:
-                notifC.SetActive(true);
-                notifC_name.text = message.senderName;
-                notifC_time.text = clockText.text;
-                notifC_msg.text = message.fullMessage.Substring(0, 12);
-                return;
+            switch(i) {
+                case 0:
+                    notifA_name.text = currentMessage.senderName;
+                    // modify here so the text is frozen in time
+                    notifA_time.text = clockText.text;
+                    notifA_msg.text = truncatedMessage;
+                    break;
 
-            default:
-                // should never be anything else other than 1, 2 or 3
-                return;
+                case 1:
+                    notifB_name.text = currentMessage.senderName;
+                    notifB_time.text = clockText.text;
+                    notifA_msg.text = truncatedMessage;
+                    break;
+                
+                case 2:
+                    notifC_name.text = currentMessage.senderName;
+                    notifC_time.text = clockText.text;
+                    notifA_msg.text = truncatedMessage;
+                    break;
+
+                default:
+                    // should never be anything else other than 1, 2 or 3
+                    return;
+            }
         }
     }
 
